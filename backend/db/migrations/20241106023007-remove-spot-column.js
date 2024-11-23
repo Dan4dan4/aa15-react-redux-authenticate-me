d10310d474bb93a87703b847a84f33b410391633
Dan4dan4 <danm44@hotmail.com>
2024-11-23 16:41:06 -0500

    fixed remove spot migration

--- backend/db/migrations/20241106023007-remove-spot-column.js
+++ backend/db/migrations/20241106023007-remove-spot-column.js
@@ -8,9 +8,16 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize){
-    const bookingTable = await queryInterface.describeTable('Bookings');
 
-    if (bookingTable && bookingTable.spot){
+    //this checks if 'spot' column exist in 'Bookings' table
+    const result = await queryInterface.sequelize.query(`
+      SELECT column_name
+      FROM information_schema.columns
+      WHERE table_name = 'Bookings' AND column_name = 'spot'
+      ${options.schema ? `AND table_schema = '${options.schema}'` : ''};
+    `);
+        //if more than 0 columns THEN it will remove
+    if (result[0].length > 0) {
       await queryInterface.removeColumn('Bookings', 'spot');
     }
   },
