5e44aa99bd511d05317c416c509af08739c05baf
Dan4dan4 <danm44@hotmail.com>
2024-11-24 21:28:17 -0500

    ph1 session actions and reducers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,28 @@ import App from './App';
 import './index.css';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+// In the component or file where you are dispatching the action
+import * as sessionActions from './store/session';
+
 
 const store = configureStore();
+
+if (import.meta.env.MODE !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions; // <-- ADD THIS LINE
+}
+
+// Dispatch the login action
+store.dispatch(
+  sessionActions.login({
+    credential: "Demo-lition",
+    password: "password"
+  })
+);
+
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,17 +1,19 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-  // ADD REDUCERS HERE
+  session: sessionReducer
 });
 
 let enhancer;
+
 if (import.meta.env.MODE === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
   const logger = (await import("redux-logger")).default;
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
