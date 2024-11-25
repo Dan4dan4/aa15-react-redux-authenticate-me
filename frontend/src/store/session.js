de462a91aebd98cf1a4b7c845a067a7b29d3057b
Dan4dan4 <danm44@hotmail.com>
2024-11-24 22:48:05 -0500

    restore session thunk action and ph1 complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,7 @@ import { csrfFetch } from './csrf';
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+//actions setuser and removeuser
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -16,6 +17,7 @@ const removeUser = () => {
   };
 };
 
+//thunks to login
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -30,8 +32,18 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+//thunks to restoreuser
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+//default state
 const initialState = { user: null };
 
+//reducers
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER:
