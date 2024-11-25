fc45866cb531428396d6271df23c4e9bc26c3b0d
Dan4dan4 <danm44@hotmail.com>
2024-11-24 21:08:10 -0500

    added auth backend  post /api/session and post /api/users

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,6 +1,7 @@
 // routes/session.js
 const express = require('express');
 const router = express.Router();
+const { User} = require('../../db/models')
 
 router.get('/', (req, res) => {
   if (req.user === null) {
@@ -17,5 +18,25 @@ router.get('/', (req, res) => {
     });
   }
 });
+
+
+// POST /api/session (Login)
+router.post('/', async (req, res) => {
+  if (req.user === null) {
+    res.json({ user: null });
+  } else {
+    res.json({
+      user: {
+        id: req.user.id,
+        firstName: req.user.firstName,
+        lastName: req.user.lastName,
+        email: req.user.email,
+        username: req.user.username,
+      }
+    });
+  }
+});
+
+
 
 module.exports = router;
