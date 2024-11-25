ca52307d88a497d7ee2ca0054f3f26fb72045afd
Dan4dan4 <danm44@hotmail.com>
2024-11-24 23:17:09 -0500

    ph2 done with signupform

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -40,6 +40,24 @@ export const restoreUser = () => async (dispatch) => {
   return response;
 };
 
+//thunks to signup
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 //default state
 const initialState = { user: null };
 
@@ -60,5 +78,6 @@ export default sessionReducer;
 export const sessionActions = {
     login,
     setUser,
-    removeUser
+    removeUser,
+    signup
   };
